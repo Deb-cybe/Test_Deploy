19a081ae040cb7140fe7f380f1228c000abfa811
Deb-cybe <101504590+Deb-cybe@users.noreply.github.com>
2023-04-04 15:45:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-// Import http library
-const http = require("http")
-// use env variable to define tcp/ip port with a default
-const PORT = process.env.PORT || 8080
-//create our server object
-const server = http.createServer();
-// We define a function that runs in response a request event
-server.on("request", (request, response) => {
-  // handle request based on method then URL
-  response.statusCode = 200;
-  response.write("Hello World...........Learning Node JS");
-  response.end();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+ 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
-// get the server to start listening
-server.listen(PORT, err => {
-    // error checking
-    err ? console.error(err) : console.log(`listening on port ${PORT}`);
-  });
+ 
+/*router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+ 
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});*/
+ 
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+ 
+console.log('Running at Port 3000');
