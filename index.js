a98b93f7c8a8f4592efc5e0c7c434b701cc28bca
Deb-cybe <101504590+Deb-cybe@users.noreply.github.com>
2023-04-07 14:33:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-
 const express = require('express');
-const app = express();
 const path = require('path');
-const router = express.Router();
- 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
- 
-/*router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
- 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});*/
- 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
- 
-console.log('Running at Port 3000');
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
